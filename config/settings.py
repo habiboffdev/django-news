77edc3ae656804761b49452955a08b88e057c4b2
Habibov Mirzosharif <83966711+4TUNA@users.noreply.github.com>
2021-08-16 19:52:38 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
