ee03e16a539de52e242890da513805ce2433f1d1
Habibov Mirzosharif <83966711+4TUNA@users.noreply.github.com>
2021-08-16 20:21:50 +0500

    y

--- news/models.py
+++ news/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
+    author = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
